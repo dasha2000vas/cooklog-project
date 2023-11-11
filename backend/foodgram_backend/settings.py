ac357a3a1e8016cfb753aa31843cce53424584fe
Дарья Василевская <vasilevsckaya.dascha@yandex.ru>
2023-11-12 00:12:50 +0300

    Return backend to postgresql

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv(
     'zjp9%de+9i*zpa20h-z80lulf@0+@q5t&zl9n7zmfx_r1w1*))'
 )
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'None').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
 
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
